2618a9001cc71d93ea3230c4d406d95b3d44e0b2
vudung45 <vudung45@yahoo.com>
2019-04-23 10:48:44 -0500

    update script

--- AutoAttendance.js
+++ AutoAttendance.js
@@ -1,6 +1,15 @@
+/*
+Instructions:
+	1. Login to your canvas, then redirect the webpage to online class video stream.
+	2. Copy the snippet below
+	3. Then just paste it inside a Chrome or Firefox developer console to run
+	4. If you see the console print ("Find Button") then the script is working
+
+This script should automatically select a poll answer whenever there is a new one popping up
+*/
 function clickButton()
 {
-    var button = document.getElementsByClassName("btn btn-primary");
+    let button = document.getElementsByClassName("btn btn-primary");
     console.log("Find Button");
     if(button.length > 0)
     {
@@ -10,3 +19,4 @@ function clickButton()
     }
     setTimeout(function(){ clickButton();}, 100);
 }
+clickButton();
